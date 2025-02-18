79df954aae76f1600b766b696cd682fb2cadb199
Diana-yjh <projin1234@gmail.com>
2025-02-18 22:10:28 +0900

    FEAT :: Deque 구현

--- Yejin/Yejin/CodeTree/13th/Deque/Deque.swift
+++ Yejin/Yejin/CodeTree/13th/Deque/Deque.swift
@@ -6,37 +6,31 @@
 //
 
 func deque() {
-    let input = Int(readLine()!)!
+    var deque = DoublyLinkedList()
+    let num = Int(readLine()!)!
     
-    for _ in 0 ..< input {
+    for _ in 0 ..< num {
+        let input = readLine()!.split(separator: " ")
         
+        switch input[0] {
+        case "push_front":
+            deque.push_front(value: Int(input[1])!)
+        case "push_back":
+            deque.push_back(value: Int(input[1])!)
+        case "pop_front":
+            print(deque.pop_front()!)
+        case "pop_back":
+            print(deque.pop_back()!)
+        case "front":
+            print(deque.front()!)
+        case "back":
+            print(deque.back()!)
+        case "empty":
+            print(deque.empty() ? 1 : 0)
+        case "size":
+            print(deque.size)
+        default:
+            print("Default")
+        }
     }
 }
-
-struct Deque {
-    let deque: [Int] = []
-    
-    var size: Int { return 0 }
-    var empty: Bool { return false }
-    
-    func push_front() -> Int? {
-        if size == 0 { return nil }
-        
-        
-    }
-    
-    func push_back() {}
-    func pop_front() {}
-    func pop_back() {}
-    func front() {}
-    func back() {}
-    
-}
-// push_front
-// push_back
-// pop_front
-// pop_back
-// size
-// empty
-// front
-// back
